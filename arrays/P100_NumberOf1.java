e197b34676a71d8a86f73e1cd8cd5253bb492b3c
liang11111 <liang11111@github.com>
2018-06-30 16:43:35 +0800

    添加P100题目15：二进制中1的个数

--- arrays/P100_NumberOf1.java
+++ arrays/P100_NumberOf1.java
@@ -1,4 +1,6 @@
-public class Test {
+package arrays;
+
+public class P100_NumberOf1 {
     public int NumberOf1(int n) {
         int count = 0;
         while(n != 0){
@@ -9,11 +11,9 @@ public class Test {
     }
     public static void main(String[] args){
         int n  = 5;
-        Test a = new Test();
+        P100_NumberOf1 a = new P100_NumberOf1();
         int b = a.NumberOf1(n);
         System.out.println(b);
 
     }
 }
-
-
