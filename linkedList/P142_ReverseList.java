407b455886dde0a9a52ee62dd49f7b0f3637002d
liang11111 <liang11111@github.com>
2018-07-02 10:40:23 +0800

    添加P142题目24：反转链表

---
+++ linkedList/P142_ReverseList.java
@@ -0,0 +1,30 @@
+package linkedList;
+
+public class P142_ReverseList {
+    public ListNode ReverList(ListNode head){
+        if(head == null){
+            return null;
+        }
+        ListNode Node = head;
+        ListNode ReverseHead = null;
+        ListNode PreNode = null;
+
+        while(Node != null){
+            ListNode NextNode = Node.next;
+            if(Node.next == null){
+                ReverseHead = Node;
+            }
+
+            Node.next = PreNode;
+
+            PreNode = Node;
+            Node = NextNode;
+
+        }
+        return ReverseHead;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
