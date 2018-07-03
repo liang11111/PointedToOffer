268c984947f8d6da7aeb95e68de52cd2cb15171c
liang11111 <liang11111@github.com>
2018-07-03 21:15:52 +0800

    增加P161题目29：顺时针打印矩阵

---
+++ Number/P161_PrintMatrixClockwise.java
@@ -0,0 +1,73 @@
+package Number;
+
+import java.util.ArrayList;
+
+public class P161_PrintMatrixClockwise {
+    public ArrayList<Integer> printMatrix(int [][] matrix) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int rows = matrix.length;
+        int cloums = matrix[0].length;
+        if( matrix == null || rows <= 0 || cloums <= 0 ){
+            return list;
+        }
+        int start = 0;
+        while(cloums > 2 * start && rows > 2* start){
+            list = PrintMatrix(matrix,rows,cloums,start);
+            start++;
+        }
+        return list;
+    }
+
+    ArrayList<Integer> list1 = new ArrayList<>();
+    public ArrayList<Integer> PrintMatrix(int[][] matrix,int rows,int cloums,int start){
+
+        int endX = cloums - 1 - start;
+        int endY = rows - 1 - start;
+        //从左到右打印一行
+        for(int i = start; i <= endX;i++){
+            int number = matrix[start][i];
+            list1.add(number);
+        }
+
+        //从上向下打印一列
+        if(endY > start){
+            for(int i = start + 1; i <= endY; i++){
+                int number = matrix[i][endX];
+                list1.add(number);
+            }
+        }
+
+        //从右向左打印一行
+        if(endX > start && endY > start){
+            for(int i = endX -1 ;i >= start; i--){
+                int number = matrix[endY][i];
+                list1.add(number);
+            }
+        }
+
+        //从下向上打印一列
+        if(endY > start + 1 && endX > start){
+            for(int i = endY - 1; i > start; i--){
+                int number = matrix[i][start];
+                list1.add(number);
+            }
+        }
+        return list1;
+    }
+
+    public  static  void main(String[] args) {
+        int[][] matrix = new int[4][4];
+        int m = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = m++;
+            }
+        }
+
+
+        P161_PrintMatrixClockwise a = new P161_PrintMatrixClockwise();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1 = a.printMatrix(matrix);
+        System.out.print(list1);
+    }
+}
--- Test.java
+++ Test.java
@@ -1,48 +1,72 @@
+import java.util.ArrayList;
 public class Test {
-    public void reOrderArray(int [] array) {
-        int count = 0;
-        //数组中奇数的个数
-        for(int i = 0; i < array.length; i++){
-            if(array[i] % 2 == 0){
-                count++;
-            }
-        }
-        //奇数和偶数分别存放在一个数组
-        int[] evenArray = new int[count];
-        int[] oddArray = new int[array.length - count];
-        int j = 0;
-        int m = 0;
-        for(int i = 0; i < array.length; i++){
-            if(array[i] % 2 == 0){
-                evenArray[j] = array[i];
-                j++;
+    public void PrintMatrixClockwisely(int[][] matrix){
+        int columns = matrix[0].length;
+        int rows = matrix.length;
 
-            }
-            else{
-                oddArray[m] = array[i];
-                m++;
-            }
-        }
-        //将奇数部分 赋值给array前部分
-        for(int i = 0;i < oddArray.length;i++){
-            array[i] = oddArray[i];
-        }
-        //将偶数部分赋值给array
-        for(int p = 0;p < evenArray.length;p++){
-            array[oddArray.length + p] = evenArray[p];
+        if(matrix == null || columns <= 0 || rows <= 0){
+            return;
         }
 
-        //打印数组array内容
-        for(int n = 0; n < array.length;n++){
-            System.out.print(array[n]);
+        int start = 0;
+        while(columns > start * 2 && rows > start * 2 ){
+            PrintMatrixCircle(matrix,columns,rows,start);
+            start++;
         }
     }
-    public static void main(String[] args){
-        int[] n  = {2};
-        Test a = new Test();
-        a.reOrderArray(n);
-//        System.out.println(b);
 
+
+
+    public void  PrintMatrixCircle(int[][] matrix, int columns, int rows, int start){
+
+
+        int endX = columns - 1 - start;
+        int endY = rows - 1 -start;
+
+        //从左到右打印一行
+        for(int i = start; i <= endX; i++){
+            int number = matrix[start][i];
+           System.out.print(number + " ");
+        }
+
+        //从上到下打印一列
+        if(start < endY){
+            for(int i = start + 1; i <= endY; i++){
+                int number = matrix[i][endX];
+                System.out.print(number + " ");
+            }
+        }
+
+        //从右向左打印一行
+        if(endX > start ){
+            for(int i = endX - 1; i >= start; i--){
+                int number = matrix[endY][i];
+                System.out.print(number + " ");
+            }
+        }
+
+        //从下向上打印一列
+        if(start < endX && start < endY - 1){
+            for(int i = endY -1; i > start; i--){
+                int number = matrix[i][start];
+                System.out.print(number + " ");
+            }
+        }
+    }
+
+
+    public  static  void main(String[] args){
+        int[][] matrix = new int[1][1];
+        int m = 1;
+        matrix[0][0] = 1;
+//        for(int i = 0;i < 4;i++){
+//            for(int j = 0;j < 4;j++){
+//                matrix[i][j] = m++;
+//            }
+
+
+        Test a = new Test();
+        a.PrintMatrixClockwisely(matrix);
     }
 }
 
